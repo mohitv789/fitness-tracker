fe4f6df0757c825555277cfa78a0b26d2ecc7fa2
Mohit Verma <mohitv15@gmail.com>
2021-07-11 16:02:33 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAS0B52UfqGoIddojKE-1s68y3EemwTGOY",
-    authDomain: "angularbackend-9ffc2.firebaseapp.com",
-    databaseURL: "https://angularbackend-9ffc2-default-rtdb.firebaseio.com",
-    projectId: "angularbackend-9ffc2",
-    storageBucket: "angularbackend-9ffc2.appspot.com",
-    messagingSenderId: "23789679652",
-    appId: "1:23789679652:web:a4a5a794792cb152ecd4b1",
-    measurementId: "G-NZG3LSBL13"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "1",
+    measurementId: ""
   }
 };
 
